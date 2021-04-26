a435715c68040b4e63e2fdea14e2f57febafb93e
侯欣榆 <xinyu.hou@dhc.com.cn>
2021-04-26 23:29:39 +0800

    明暗主题切换

--- vue.config.js
+++ vue.config.js
@@ -46,12 +46,11 @@ module.exports = {
     // .loader('image-webpack-loader')
     // .options({bypassOnDebug: true})
     // .end()
-
     // 开启js、css压缩
     if (process.env.NODE_ENV === 'production') {
       config.plugin('compressionPlugin')
         .use(new CompressionPlugin({
-          test: /\.js$|\.html$|.\css/, // 匹配文件名
+          test: /\.js$|\.html$|\.css/, // 匹配文件名
           threshold: 10240, // 对超过10k的数据压缩
           deleteOriginalAssets: false // 不删除源文件
         }))
